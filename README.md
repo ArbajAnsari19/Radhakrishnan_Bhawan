2d024a8cea805ce4574ed95f6a6b7a8fa6d30697
Arbaj Ansari <a_ansari@mt.iitr.ac.in>
2024-02-29 23:12:33 +0530

    first commit

--- README.md
+++
@@ -1,3 +0,0 @@
-# IITR_Bhawan_Website
-# Bhawan_IITR
-# Bhawan_IITR
--- rkb.js
+++ rkb.js
@@ -37,3 +37,70 @@ document.querySelector("#btn4").addEventListener("click", function(e)
     document.getElementById("b_cards2023").style.display = "none"; 
 })
 
+
+
+
+let phla = document.querySelector(".phla");
+let burger = document.querySelector("#burger");
+let flag = false;
+
+document.addEventListener('DOMContentLoaded',function()
+{
+
+burger.addEventListener('click', handleClick);
+
+
+function handleClick()
+{
+    if(flag===false)
+    {
+      phla.style.transform = "translateX(-50%)";
+      flag = true;
+    }
+    else{
+      phla.style.transform = "translateX(0%)";
+      flag = false;
+    }
+   
+}
+})
+
+//
+
+var prev = document.getElementById('prev');
+var next = document.getElementById('next');
+var slider = document.getElementById('slider');
+var total = 0, step = 100;
+
+prev.addEventListener('click', slide);
+next.addEventListener('click', slide);
+
+function slide()
+{
+  if(this.getAttribute('id') == 'prev')
+  {
+    if(total == 0)
+    {
+      total = -400;
+      slider.style.left = total + '%';
+    }
+    else
+    {
+      total += step;
+      slider.style.left = total + '%';
+    }
+  }
+  else
+  {
+    if(total == -400)
+    {
+      total = 0;
+      slider.style.left = total;
+    }
+    else
+    {
+      total -= step;
+      slider.style.left = total + '%';
+    }
+  }
+}
